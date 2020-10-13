b2194c8b18b8261801f8e04b6b4d5eabfbb5b45c
jomo0825 <jomo0825@gmail.com>
2020-10-13 11:00:13 +0800

    20201013 1100

--- Assets/ActorController.cs
+++ Assets/ActorController.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ActorController : MonoBehaviour
 {
+    //public variables
     public PlayerInput pi;
-    public Animator anim;
+    public GameObject model;
+    public float walkSpeed = 1.0f;
+
+    // private variables   
+    private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;
 
 
     // Start is called before the first frame update
@@ -18,18 +23,27 @@ public class ActorController : MonoBehaviour
             return;
         }
 
-        //pi = GetComponent<PlayerInput>();
-        //if (pi == null)
-        //{
-        //    pi = gameObject.AddComponent<PlayerInput>();
-        //}
-
         pi = GetComponent<PlayerInput>()? GetComponent<PlayerInput>(): gameObject.AddComponent<PlayerInput>();
+        model = anim.gameObject;
+        rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame, 60Hz
     void Update()
     {
         anim.SetFloat("forward", pi.Dmag);
+
+        if (pi.Dmag > 0.01f)
+        {
+            model.transform.forward = this.transform.TransformVector(pi.Dvec);
+        }
+
+        movingVec = pi.Dmag * model.transform.forward * walkSpeed;
+    }
+
+    // 50Hz
+    void FixedUpdate()
+    {
+        rigid.velocity = movingVec;
     }
 }
