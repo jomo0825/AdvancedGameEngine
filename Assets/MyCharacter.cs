e503c02db41e78e921d31be4cedabbfc4af70e5c
jomo0825 <jomo0825@gmail.com>
2020-12-29 12:03:42 +0800

    20201229 1203

---
+++ Assets/MyCharacter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kryz.CharacterStats.Examples;
+
+public class MyCharacter : MonoBehaviour
+{
+    public EquippableItem itemPrefab1;
+    public Inventory inv;
+    public GameObject invCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            invCanvas.SetActive(!invCanvas.activeSelf);
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            inv.AddItem(itemPrefab1);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            inv.RemoveItem(itemPrefab1);
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            if(inv.SearchItem(itemPrefab1) == true)
+            {
+                print("you got MySword");
+            }
+            else
+            {
+                print("you don't have MySword.");
+            }
+        }
+    }
+}
---
+++ Assets/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kryz.CharacterStats.Examples;
+
+public class Pickup : MonoBehaviour
+{
+    public EquippableItem itemPrefab    ;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MyCharacter my = other.GetComponent<MyCharacter>();
+        if (my == null)
+        {
+            return;
+        }
+        my.inv.AddItem(itemPrefab);
+        Destroy(gameObject);
+        // Explode particle here.
+    }
+}
